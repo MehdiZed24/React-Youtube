793d80803eb07f9d53dfa4d8a437b454345498ba
MehdiZed24 <mehdiziani1989@gmail.com>
2021-09-14 19:30:50 +0200

    Apprentissage de la navigation

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A Propos</h1>
+      <br/>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat architecto eligendi iusto, laborum libero odio nostrum expedita, commodi suscipit illo modi maxime, ab perferendis? Quam corporis eligendi corrupti et? Impedit, aut dolorum enim saepe repellat voluptatum facere beatae inventore minima necessitatibus recusandae consectetur repudiandae error quae deleniti, ullam repellendus commodi fuga quas? Mollitia rerum quaerat officiis dolor? Tempore, adipisci illum?</p>
+      <br/>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat architecto eligendi iusto, laborum libero odio nostrum expedita, commodi suscipit illo modi maxime, ab perferendis? Quam corporis eligendi corrupti et? Impedit, aut dolorum enim saepe repellat voluptatum facere beatae inventore minima necessitatibus recusandae consectetur repudiandae error quae deleniti, ullam repellendus commodi fuga quas? Mollitia rerum quaerat officiis dolor? Tempore, adipisci illum?</p>
+    </div>
+  );
+};
+
+export default About;
